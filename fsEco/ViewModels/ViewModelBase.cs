df1af7126ac3b227ab231e5175fa32a83509c018
Stalkero <Stalkero@stalkeroserwer.com>
2025-06-22 00:10:03 +0200

    Add project files.

---
+++ fsEco/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace fsEco.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ fsEco/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace fsEco.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
