5e3e9bd1cb6c0ca7b100da93335c68fb26af5659
Stalkero <Stalkero@stalkeroserwer.com>
2025-06-22 04:08:11 +0200

    Added loading airports from csv file

--- fsEco/Views/MainWindow.axaml.cs
+++ fsEco/Views/MainWindow.axaml.cs
@@ -2,9 +2,13 @@
 using CsvHelper;
 using fsEco.Classes;
 using fsEco.Utils.Windows;
+using System;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
+using static fsEco.Utils.Windows.ErrorWindow;
+
 
 namespace fsEco.Views;
 
@@ -13,6 +17,21 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+
+        try
+        {
+            using var reader = new StreamReader("airports.csv");
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            var airports = csv.GetRecords<fsEco.Classes.airport>().ToList();
+
+            new ErrorWindow($"Airports loaded").Show();
+        }
+        catch (Exception ex)
+        {
+            new ErrorWindow($"Error occurred while loading airport csv. {ex.ToString()}").Show();
+        }
+
 
 
         MainContent.Content = new MainView();
